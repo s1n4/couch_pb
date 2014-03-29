cc4fab9ba7958e697439323530a8c29fc8a99f0e
Sina Samavati <sina.samv@gmail.com>
2014-03-29 23:43:12 +0430

    Add couch_pb_codec:get_command_info/1, get_command_mfa/2

--- src/couch_pb_codec.erl
+++ src/couch_pb_codec.erl
@@ -1,17 +1,25 @@
 -module(couch_pb_codec).
--compile(export_all).
+
+-export([get_command_info/1]).
+-export([get_command_mfa/2]).
 
 -include("include/couch_pb_codec.hrl").
 
--spec command(non_neg_integer() | atom()) -> atom() | non_neg_integer() |
-                                             {error, not_found}.
-command(N) when is_integer(N) ->
+-type command_info() :: {non_neg_integer(), atom(), {module(), atom()}}.
+
+-spec get_command_info(non_neg_integer() | atom()) -> command_info() |
+                                                      {error, not_found}.
+get_command_info(N) when is_integer(N) ->
     case lists:keyfind(N, 1, ?COMMANDS) of
-        {N, C, _} -> C;
+        X = {N, _, _} ->  X;
         _ -> {error, not_found}
     end;
-command(C) when is_atom(C) ->
+get_command_info(C) when is_atom(C) ->
     case lists:keyfind(C, 2, ?COMMANDS) of
-        {N, C, _} -> N;
+        X = {_, C, _} -> X;
         _ -> {error, not_found}
     end.
+
+-spec get_command_mfa(command_info(), [any()]) -> mfa().
+get_command_mfa({_, _, {M, F}}, A) ->
+    {M, F, A}.
