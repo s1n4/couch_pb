4d1464af6825eea6f75f9cf18a4b81bd34e57772
Sina Samavati <sina.samv@gmail.com>
2014-03-30 04:25:07 +0430

    Add couch_pb_codec:encode/2, decode/2

--- src/couch_pb_codec.erl
+++ src/couch_pb_codec.erl
@@ -1,11 +1,26 @@
 -module(couch_pb_codec).
 
+-export([encode/2]).
+-export([decode/2]).
 -export([get_command_info/1]).
 -export([get_command_mfa/2]).
 
+-include("include/couch_pb.hrl").
 -include("include/couch_pb_codec.hrl").
 
 -type command_info() :: {non_neg_integer(), atom(), {module(), atom()}}.
+
+-spec encode(req | resp, #cpbrequest{} | #cpbresponse{}) -> binary().
+encode(req, Req) ->
+    iolist_to_binary(couch_pb:encode_cpbrequest(Req));
+encode(resp, Resp) ->
+    iolist_to_binary(couch_pb:encode_cpbresponse(Resp)).
+
+-spec decode(req | resp, binary()) -> #cpbrequest{} | #cpbresponse{}.
+decode(req, Req) ->
+    couch_pb:decode_cpbrequest(Req);
+decode(resp, Resp) ->
+    couch_pb:decode_cpbresponse(Resp).
 
 -spec get_command_info(non_neg_integer() | atom()) -> command_info() |
                                                       {error, not_found}.
