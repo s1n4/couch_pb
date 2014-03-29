2a17f340afbd3dfcbc9b5a165a1aa2620607a122
Sina Samavati <sina.samv@gmail.com>
2014-03-29 22:38:26 +0430

    Update codec table

--- src/couch_pb_codec.erl
+++ src/couch_pb_codec.erl
@@ -6,12 +6,12 @@
 -spec command(non_neg_integer() | atom()) -> atom() | non_neg_integer() |
                                              {error, not_found}.
 command(N) when is_integer(N) ->
-    case lists:keyfind(N, 2, ?COMMANDS) of
-        {C, N} -> C;
+    case lists:keyfind(N, 1, ?COMMANDS) of
+        {N, C, _} -> C;
         _ -> {error, not_found}
     end;
 command(C) when is_atom(C) ->
-    case lists:keyfind(C, 1, ?COMMANDS) of
-        {C, N} -> N;
+    case lists:keyfind(C, 2, ?COMMANDS) of
+        {N, C, _} -> N;
         _ -> {error, not_found}
     end.
