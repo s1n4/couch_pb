5ab2f9f179b5ce81a0cda6902734bfb687d5e1a8
Sina Samavati <sina.samv@gmail.com>
2014-03-31 20:59:13 +0430

    Change commands to {num, name}

--- src/couch_pb_codec.erl
+++ src/couch_pb_codec.erl
@@ -2,13 +2,11 @@
 
 -export([encode/2]).
 -export([decode/2]).
--export([get_command_info/1]).
--export([get_command_mfa/2]).
+-export([get_command_name/1]).
+-export([get_command_num/1]).
 
 -include("include/couch_pb.hrl").
 -include("include/couch_pb_codec.hrl").
-
--type command_info() :: {non_neg_integer(), atom(), {module(), atom()}}.
 
 -spec encode(req | resp, #cpbrequest{} | #cpbresponse{}) -> binary().
 encode(req, Req) ->
@@ -22,19 +20,16 @@ decode(req, Req) ->
 decode(resp, Resp) ->
     couch_pb:decode_cpbresponse(Resp).
 
--spec get_command_info(non_neg_integer() | atom()) -> command_info() |
-                                                      {error, not_found}.
-get_command_info(N) when is_integer(N) ->
+-spec get_command_name(non_neg_integer()) -> atom() | {error, not_found}.
+get_command_name(N) ->
     case lists:keyfind(N, 1, ?COMMANDS) of
-        X = {N, _, _} ->  X;
-        _ -> {error, not_found}
-    end;
-get_command_info(C) when is_atom(C) ->
-    case lists:keyfind(C, 2, ?COMMANDS) of
-        X = {_, C, _} -> X;
+        {N, C} ->  C;
         _ -> {error, not_found}
     end.
 
--spec get_command_mfa(command_info(), [any()]) -> mfa().
-get_command_mfa({_, _, {M, F}}, A) ->
-    {M, F, A}.
+-spec get_command_num(atom()) -> non_neg_integer() | {error, not_found}.
+get_command_num(C) ->
+    case lists:keyfind(C, 2, ?COMMANDS) of
+        {N, C} -> N;
+        _ -> {error, not_found}
+    end.
