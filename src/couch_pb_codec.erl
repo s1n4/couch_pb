f7820f1c53bd4b2e8d3721e806bba3947f1e3757
Sina Samavati <sina.samv@gmail.com>
2014-04-04 14:20:38 +0430

    Rename couch_pb_codec:get_command_num/1 to get_command_id/1
    add more tests for get_command_id/1

--- src/couch_pb_codec.erl
+++ src/couch_pb_codec.erl
@@ -13,7 +13,7 @@
 -export([decode_req/1]).
 -export([decode_resp/1]).
 -export([get_command_name/1]).
--export([get_command_num/1]).
+-export([get_command_id/1]).
 
 -include("include/couch_pb.hrl").
 
@@ -40,8 +40,8 @@ get_command_name(N) ->
         _ -> {error, not_found}
     end.
 
--spec get_command_num(atom()) -> non_neg_integer() | {error, not_found}.
-get_command_num(C) ->
+-spec get_command_id(atom()) -> non_neg_integer() | {error, not_found}.
+get_command_id(C) ->
     case lists:keyfind(C, 2, commands()) of
         {N, C} -> N;
         _ -> {error, not_found}
@@ -81,9 +81,15 @@ decode_resp_test() ->
     true = (decoded_resp() =:= decode_resp(encoded_resp())).
 
 get_command_name_test() ->
-    all_dbs = get_command_name(2).
+    true = is_atom(get_command_name(2)),
+    true = is_atom(get_command_name(21)),
+    {error, not_found} = get_command_name(-86),
+    {error, not_found} = get_command_name(wat_da___).
 
-get_command_num_test() ->
-    2 = get_command_num(all_dbs).
+get_command_id_test() ->
+    true = is_integer(get_command_id(all_dbs)),
+    true = is_integer(get_command_id(stats)),
+    {error, not_found} = get_command_id(wut),
+    {error, not_found} = get_command_id(-69).
 
 -endif.
